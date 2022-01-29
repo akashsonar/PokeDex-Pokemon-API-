b74c1c47ba941b24bc4a6f1de4817ad4e766c4a8
akash sonar <sonarakash888@gmail.com>
2022-01-29 15:41:45 +0530

    image data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Axios from 'axios';
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import "./app.scss";
 
 export default function App() {
@@ -41,28 +41,28 @@ export default function App() {
         <div className="app">
             <div className="titleSection">
 
-                <img className="title" src="https://i.imgur.com/k4tYktt.png"/>
-                <img className="pokedex" src="https://bit.ly/3u4iJAe"/>
+                <img className="title" src="https://i.imgur.com/k4tYktt.png" alt=""/>
+                <img className="pokedex" src="https://bit.ly/3u4iJAe" alt=""/>
                 <input
                     type="text"
                     onChange={(e) => {
                     setPokemonName(e.target.value)
                 }}/>
-                <button onClick={searchPokemon}><img className="search" src="https://i.imgur.com/OR1qVRj.png"/></button>
+                <button onClick={searchPokemon}><img className="search" src="https://i.imgur.com/OR1qVRj.png" alt=""/></button>
             </div>
             <div className="displaySection">
                 {!pokeChoosen
-                    ? (<img className="choosePokemon" src="https://i.imgur.com/AM9pxUH.png"/>)
+                    ? (<img className="choosePokemon" src="https://i.imgur.com/AM9pxUH.png" alt=""/>)
                     : (
                         <div className="info">
                             <h1>{pokeInfo.name}</h1>
-                            <img src={pokeInfo.img}/>
-                            <h3><img src="https://i.imgur.com/Fr9dssX.png" border="0"/>
+                            <img src={pokeInfo.img} alt=""/>
+                            <h3><img src="https://i.imgur.com/Fr9dssX.png" border="0" alt=""/>
                                 : {pokeInfo.species}</h3>
-                            <h3><img src="https://i.imgur.com/12t2a44.png" border="0"/>: {pokeInfo.type}</h3>
-                            <h3><img src="https://i.imgur.com/MiBfnDx.png" border="0"/>: {pokeInfo.hp}</h3>
-                            <h3><img src="https://i.imgur.com/OugaI5a.png" border="0"/>: {pokeInfo.attack}</h3>
-                            <h3><img src="https://i.imgur.com/VjcRo1v.png" border="0"/>: {pokeInfo.defence}</h3>
+                            <h3><img src="https://i.imgur.com/12t2a44.png" border="0" alt=""/>: {pokeInfo.type}</h3>
+                            <h3><img src="https://i.imgur.com/MiBfnDx.png" border="0" alt=""/>: {pokeInfo.hp}</h3>
+                            <h3><img src="https://i.imgur.com/OugaI5a.png" border="0" alt=""/>: {pokeInfo.attack}</h3>
+                            <h3><img src="https://i.imgur.com/VjcRo1v.png" border="0" alt=""/>: {pokeInfo.defence}</h3>
                         </div>
                     )}
             </div>
